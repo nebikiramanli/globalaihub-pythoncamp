280e7da9bd53c8f12a17ac56b5c93cdb8080d938
Nebi Kıramanlıoğlu <nebikiramanli@gmail.com>
2022-06-28 23:10:02 +0300

    rank#4 hacker rank 4 yapıldı.

---
+++ hackerrank.4/division.py
@@ -0,0 +1,11 @@
+def sub_division(a, b):
+    return a // b
+
+def division(a, b):
+    return a / b
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(sub_division(a, b))
+    print(division(a, b))
